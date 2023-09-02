16642701ff5c518693be3a660dffa29ed193fe25
Your Name <you@example.com>
2023-09-02 12:17:22 +0100

    bach

--- option_pricers/bachelier.py
+++ option_pricers/bachelier.py
@@ -34,7 +34,7 @@ class bachelier_torch(PriceModel):
     def __init__(self, S_0, strike, time_to_expiry, implied_vol, dividend, riskfree_rate, option_sign):
         super().__init__(S_0, strike, time_to_expiry, implied_vol, dividend, riskfree_rate, option_sign, 'Black76')
         
-        self.gradient = None
+        # self.gradient = None
         self.npv_pytorch = None
         self.greeks = None
 
